10e95f8d6461de79d73540029fba22c762910d30
Buddha <jyothiprasadb@gmail.com>
2015-02-05 09:29:09 +0530

    additional UI fields required by lazyBones template

--- impl/src/main/java/org/codehaus/griffon/forge/ui/setup/SetupCommand.java
+++ impl/src/main/java/org/codehaus/griffon/forge/ui/setup/SetupCommand.java
@@ -1,18 +1,17 @@
 package org.codehaus.griffon.forge.ui.setup;
 
-import java.util.logging.Logger;
-
-import javax.inject.Inject;
-
 import org.codehaus.griffon.forge.GriffonFacet;
 import org.codehaus.griffon.forge.ui.AbstractGriffonCommand;
 import org.codehaus.griffon.types.FrameworkTypes;
 import org.codehaus.griffon.types.LanguageTypes;
 import org.jboss.forge.addon.facets.FacetFactory;
+import org.jboss.forge.addon.projects.Project;
+import org.jboss.forge.addon.resource.DirectoryResource;
 import org.jboss.forge.addon.ui.context.UIBuilder;
 import org.jboss.forge.addon.ui.context.UIContext;
 import org.jboss.forge.addon.ui.context.UIExecutionContext;
 import org.jboss.forge.addon.ui.hints.InputType;
+import org.jboss.forge.addon.ui.input.UIInput;
 import org.jboss.forge.addon.ui.input.UISelectOne;
 import org.jboss.forge.addon.ui.metadata.UICommandMetadata;
 import org.jboss.forge.addon.ui.metadata.WithAttributes;
@@ -20,6 +19,9 @@ import org.jboss.forge.addon.ui.result.Result;
 import org.jboss.forge.addon.ui.result.Results;
 import org.jboss.forge.addon.ui.util.Categories;
 import org.jboss.forge.addon.ui.util.Metadata;
+
+import javax.inject.Inject;
+import java.util.logging.Logger;
 
 public class SetupCommand extends AbstractGriffonCommand {
 
@@ -39,17 +41,37 @@ public class SetupCommand extends AbstractGriffonCommand {
 	private UISelectOne<LanguageTypes> languageType;
 
 	@Inject
+	@WithAttributes(label = "Maven Group", defaultValue = "org.example", shortName = 'g')
+	private UIInput<String> group;
+
+	@Inject
+	@WithAttributes(label = "Maven Version", defaultValue = "0.1.0-SNAPSHOT", shortName = 'm')
+	private UIInput<String> mavenVersion;
+
+	@Inject
+	@WithAttributes(label = "Maven Version", defaultValue = "org.example", shortName = 'p')
+	private UIInput<String> basePackage;
+
+	@Inject
 	private FacetFactory facetFactory;
 
 	@Override
 	public Result execute(UIExecutionContext context) throws Exception {
+		Project selectedProject = getSelectedProject(context);
+		DirectoryResource directoryResource = (DirectoryResource)selectedProject.getRoot();
+		directoryResource.getOrCreateChildDirectory("config");
 
-		return Results.success("Griffon has been installed.");
+		return Results.success("Your project is modified as a Griffon Project");
 	}
 
 	@Override
 	public void initializeUI(UIBuilder builder) throws Exception {
-		builder.add(griffonVersion).add(frameworkType).add(languageType);
+		builder.add(griffonVersion)
+			   .add(frameworkType)
+				.add(languageType)
+				.add(group)
+				.add(mavenVersion)
+				.add(basePackage);
 	}
 
 	@Override
