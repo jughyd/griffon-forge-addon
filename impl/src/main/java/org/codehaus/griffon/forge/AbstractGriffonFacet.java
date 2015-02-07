0013664728a64d8250ae892027350d3b7317658f
Buddha <jyothiprasadb@gmail.com>
2015-02-08 01:10:30 +0530

    adding the required dependencies to pom.xml and folders in root

--- impl/src/main/java/org/codehaus/griffon/forge/AbstractGriffonFacet.java
+++ impl/src/main/java/org/codehaus/griffon/forge/AbstractGriffonFacet.java
@@ -1,11 +1,10 @@
 package org.codehaus.griffon.forge;
 
-import javax.inject.Inject;
-
 import org.codehaus.griffon.types.FrameworkTypes;
 import org.codehaus.griffon.types.LanguageTypes;
 import org.jboss.forge.addon.dependencies.Coordinate;
 import org.jboss.forge.addon.dependencies.DependencyQuery;
+import org.jboss.forge.addon.dependencies.DependencyRepository;
 import org.jboss.forge.addon.dependencies.DependencyResolver;
 import org.jboss.forge.addon.dependencies.builder.DependencyBuilder;
 import org.jboss.forge.addon.dependencies.builder.DependencyQueryBuilder;
@@ -16,69 +15,95 @@ import org.jboss.forge.addon.projects.ProjectFacet;
 import org.jboss.forge.addon.projects.dependencies.DependencyInstaller;
 import org.jboss.forge.addon.resource.DirectoryResource;
 
+import javax.inject.Inject;
 import java.util.List;
 
 public abstract class AbstractGriffonFacet extends AbstractFacet<Project>
-		implements ProjectFacet, GriffonFacet {
+        implements ProjectFacet, GriffonFacet {
 
-	private final DependencyInstaller installer;
+    private static final DependencyRepository dependencyRepository = new DependencyRepository("jcenter", "http://jcenter.bintray.com/");
 
-	FrameworkTypes framework;
-	LanguageTypes language;
+    public static final String GRIFFON_JAVAFX = "org.codehaus.griffon:griffon-javafx";
+    public static final String GRIFFON_GUICE = "org.codehaus.griffon:griffon-guice";
+    public static final String GRIFFON_CORE_TEST = "org.codehaus.griffon:griffon-core-test";
+    public static final String GROOVY_ALL = "org.codehaus.groovy:groovy-all";
+    public static final String LOG4J = "log4j:log4j";
+    public static final String SLF4J_LOG4J12 = "org.slf4j:slf4j-log4j12";
+    public static final String SPOCK_CORE = "org.spockframework:spock-core";
+    public static final String JUNIT = "junit:junit";
+    public static final String GRIFFON_CORE_COMPILE = "org.codehaus.griffon:griffon-core-compile";
 
-	@Inject
-	public AbstractGriffonFacet(final DependencyInstaller installer) {
-		this.installer = installer;
-	}
+    private DependencyBuilder builder;
+    private DependencyInstaller installer;
 
-	@Override
-	public boolean install() {
-		createFolders();
-		addDependencies();
-		return true;
-	}
+    FrameworkTypes framework;
+    LanguageTypes language;
 
-	@Override
-	public boolean isInstalled()
-	{
-//		Project selectedProject = getFaceted();
-//		selectedProject.ge
-		return false;
-	}
+    @Inject
+    private DependencyResolver dependencyResolver;
 
-	@Override
-	public String toString() {
-		return getVersion().toString();
-	}
+    @Inject
+    public AbstractGriffonFacet(final DependencyInstaller installer) {
+        this.installer = installer;
+    }
 
-	private void createFolders(){
-		Project selectedProject = getFaceted();
-		DirectoryResource directoryResource = (DirectoryResource)selectedProject.getRoot();
-		directoryResource.getOrCreateChildDirectory("config");
+    @Override
+    public boolean install() {
+        createFolders();
+        addDependencies();
+        return true;
+    }
 
-	}
+    @Override
+    public boolean isInstalled() {
+        return false;
+    }
 
-	private void addDependencies(){
+    @Override
+    public String toString() {
+        return getVersion().toString();
+    }
 
-	}
+    private void createFolders() {
+        Project selectedProject = getFaceted();
+        DirectoryResource directoryResource = (DirectoryResource) selectedProject.getRoot();
+        directoryResource.getOrCreateChildDirectory("griffon-app");
+        directoryResource.getOrCreateChildDirectory("maven");
+        directoryResource.getOrCreateChildDirectory("config");
+    }
 
-	@Override
-	public FrameworkTypes getFramework() {
-		return framework;
-	}
+    private void addDependencies() {
+        builder = DependencyBuilder.create();
 
-	@Override
-	public void setFramework(FrameworkTypes framework) {
-		this.framework = framework;
-	}
+        addDependency(GRIFFON_CORE_COMPILE);
+        addDependency(GRIFFON_JAVAFX);
+        addDependency(GRIFFON_GUICE);
+        addDependency(GRIFFON_CORE_TEST);
+        addDependency(GROOVY_ALL);
+        addDependency(LOG4J);
+        addDependency(SLF4J_LOG4J12);
+        addDependency(SPOCK_CORE);
+        addDependency(JUNIT);
+    }
 
-	@Override
-	public LanguageTypes getLanguage() {
-		return language;
-	}
+    private void addDependency(String coordinate) {
+        DependencyQuery query = DependencyQueryBuilder
+                .create(coordinate)
+                .setFilter(new NonSnapshotDependencyFilter())
+                .setRepositories(dependencyRepository);
+        List<Coordinate> coordinates = dependencyResolver.resolveVersions(query);
+        builder.setCoordinate(coordinates.get(coordinates.size() - 1));
 
-	@Override
-	public void setLanguage(LanguageTypes language) {
-		this.language = language;
-	}
+        installer.install(getFaceted(), builder);
+    }
+
+    @Override
+    public void setFramework(FrameworkTypes framework) {
+        this.framework = framework;
+    }
+
+    @Override
+    public void setLanguage(LanguageTypes language) {
+        this.language = language;
+    }
 }
